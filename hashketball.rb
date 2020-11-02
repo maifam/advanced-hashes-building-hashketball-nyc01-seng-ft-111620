20a4b9efcecf533e528b31eb9197e3683fbf28b4
Mai Pham <maitphm@gmail.com>
2020-11-02 22:48:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,14 +1,36 @@
 def game_hash 
   basketball = { 
     :home => {
-      :team_name => " "
-      :colors => [" "]
+      :team_name => "Brooklyn Nets"
+      :colors => [""]
       :players => [{ }]
     }
     :away => {
-      :team_name =>
-      :colors =>
-      :players => 
+      :team_name => "Charlotte Hornets"
+      :colors => [""]
+      :players => [ {
+        :player_name =>
+        :number =>
+        :shoe => 
+        :pointes =>
+        :rebounds =>
+        :assists =>
+        :steals => 
+        :blocks => 
+        :slam_dunks => 
+      }, 
+      {
+        :player_name =>
+        :number =>
+        :shoe => 
+        :pointes =>
+        :rebounds =>
+        :assists =>
+        :steals => 
+        :blocks => 
+        :slam_dunks => 
+      
+      }]
       
     }
   }
