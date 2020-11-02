5cadb31988162318a8dc1286fabd13e1567021da
Mai Pham <maitphm@gmail.com>
2020-11-02 22:56:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -87,7 +87,19 @@ def game_hash
         :slam_dunks => 10 
       }, 
       { 
+        :player_name => "DeSagna Diop",
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5
+      },
+      {
         
+      }
       
       }]
       
