d08dd65f94230f4614af35a4c3bc98ded3cde829
Mai Pham <maitphm@gmail.com>
2020-11-02 22:50:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,20 @@ def game_hash
   basketball = { 
     :home => {
       :team_name => "Brooklyn Nets"
-      :colors => [""]
-      :players => [{ }]
+      :colors => ["Black", "White"]
+      :players => [ { 
+        :player_name => "Alan Anderson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists =>12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1 
+      ]
+        
+      }]
     }
     :away => {
       :team_name => "Charlotte Hornets"
@@ -12,7 +24,7 @@ def game_hash
         :player_name =>
         :number =>
         :shoe => 
-        :pointes =>
+        :points =>
         :rebounds =>
         :assists =>
         :steals => 
